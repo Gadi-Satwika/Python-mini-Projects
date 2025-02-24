8cad6ddc713655523cd72d3de5537dc6e9abf6f4
Gadi <satwikagadi2005@gmail.com>
2025-02-24 12:56:41 +0530

    Inital commit - Number Guessing Game

---
+++ guess_number_game.py
@@ -0,0 +1,14 @@
+import random
+
+def guess(x):
+	number = random.randint(1,x)
+	guess = 0
+	while(guess!=number):
+		guess = int(input("Guess the  Secret Number?(Between 1- 100) "))
+		if(guess>number):
+			print("You are too high!")
+		elif(guess<number):
+			print("You are too low!")
+		else:
+			print("Yay! You found the number")
+guess(100) 
